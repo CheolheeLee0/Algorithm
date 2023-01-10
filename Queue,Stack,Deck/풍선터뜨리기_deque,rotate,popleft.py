0ad11f7dcc67ded40e54424c3fd1deeca87fa60d
CheolheeLee0 <wks0968@gmail.com>
2023-01-10 18:06:49 +0900

    fix: 풍선 터뜨리기_deque,rotate,popleft

--- Queue,Stack,Deck/풍선터뜨리기_deque,rotate,popleft.py
+++ Queue,Stack,Deck/풍선터뜨리기_deque,rotate,popleft.py
@@ -2,9 +2,6 @@
 # Problem : https://www.acmicpc.net/problem/2346
 # 01/10 17:36
 
-# 1 ~ N 풍선 / 순환 리스트 / rotate / 
-# 종이 (-N ~ N, 2N + 1) / 종이값만큼 이동 / + : 오른쪽, - : 왼쪽
-
 import sys
 from collections import deque
 
