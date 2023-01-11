36209ee92ffd1e0637d0c8b290e88a0de4affa2a
CheolheeLee0 <wks0968@gmail.com>
2023-01-11 11:50:40 +0900

    fix: 전자레인지_빼면서나머지0체크

--- Greedy/전자레인지_빼면서나머지0체크.py
+++ Greedy/전자레인지_빼면서나머지0체크.py
@@ -8,24 +8,13 @@ from collections import deque
 def input():
   return sys.stdin.readline().rstrip()
 
-A, B, C = 300, 60, 10
-A_cnt, B_cnt, C_cnt = 0, 0, 0
-
 T = int(input())
 
-while T >= 0:
-  if T % 300 == 0:
-    A_cnt = T // 300
-    T = 0
-    break
-  elif T < 300 and T % 60 == 0:
-    B_cnt = T // 60
-    T = 0
-    break
-  T -= 10
-  C_cnt += 1
-
-if T == 0:
-  print(f'{A_cnt} {B_cnt} {C_cnt}')
+if T % 10 != 0:
+    print(-1)
 else:
-  print(-1)
+    A = B = C = 0
+    A = T // 300
+    B = (T % 300) // 60
+    C = (T % 300) % 60 // 10
+    print(A, B, C)
